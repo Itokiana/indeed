ed122a27b9398338d951ed7c807df11edffc2462
Itokiana <itokianarajoelison@gmail.com>
2019-04-27 21:56:35 +0300

    Finalised

--- scrap_indeed.rb
+++ scrap_indeed.rb
@@ -10,7 +10,7 @@ def get_french_cities
   cities
 end
 
-def get_result_cols_from_indeed(city)
+def get_result_links_from_indeed(city)
   pages = ["10","20","30","40","50"]
   doc = Nokogiri::HTML(open("https://www.indeed.fr/emplois?q=commercial+et%2Fou+rh&l=" + city))
   links = []
@@ -38,10 +38,94 @@ def get_result_cols_from_indeed(city)
   links
 end
 
+def get_result_content_from_indeed(city)
+  pages = ["10","20","30","40","50"]
+  results = []
+  titles = []
+  enterprises = []
+  regions = []
+  descriptions = []
+  doc = Nokogiri::HTML(open("https://www.indeed.fr/emplois?q=commercial+et%2Fou+rh&l=" + city))
+  i = 0
+
+  get_titles = doc.css("td#resultsCol div div.title a")
+  get_enterprises = doc.css("td#resultsCol div.sjcl span.company")
+  get_regions = doc.css("td#resultsCol div.sjcl span.location")
+  get_descriptions = doc.css("td#resultsCol div.summary")
+
+  get_titles.each do |title|
+    titles.push(title.content[13..-1])
+  end
+
+  get_enterprises.each do |e|
+    enterprises.push(e.content.delete("\n").delete(" "))
+  end
+
+  get_regions.each do |r|
+    regions.push(r.content)
+  end
+
+  get_descriptions.each do |d|
+    descriptions.push(d.content[13..-1])
+  end  
+
+  titles.each do |t|
+    results.push({
+      :title => t, 
+      :enterprise => enterprises[i], 
+      :region => regions[i], 
+      :description => descriptions[i] 
+    })
+    i = i + 1
+  end
+
+  pages.each do |p|
+    titles = []
+    enterprises = []
+    regions = []
+    descriptions = []
+    doc = Nokogiri::HTML(open("https://www.indeed.fr/emplois?q=commercial+et%2Fou+rh&l=" + city + "&start="+ p))
+    i = 0
+    get_titles = doc.css("td#resultsCol div div.title a")
+    get_enterprises = doc.css("td#resultsCol div.sjcl span.company")
+    get_regions = doc.css("td#resultsCol div.sjcl span.location")
+    get_descriptions = doc.css("td#resultsCol div.summary")
+
+    get_titles.each do |title|
+      titles.push(title.content[13..-1])
+    end
+
+    get_enterprises.each do |e|
+      enterprises.push(e.content.delete("\n").delete(" "))
+    end
+
+    get_regions.each do |r|
+      regions.push(r.content)
+    end
+
+    get_descriptions.each do |d|
+      descriptions.push(d.content[13..-1])
+    end  
+
+    titles.each do |t|
+      results.push({
+        :title => t, 
+        :enterprise => enterprises[i], 
+        :region => regions[i], 
+        :description => descriptions[i] 
+      })
+      i = i + 1
+    end
+  end
+
+  results
+end
+
 
 
 # json = JSON.pretty_generate(get_french_cities())
 # File.open("data.json", 'w') { |file| file.write(json) }
-a = get_result_cols_from_indeed('Nice')
+# a = get_result_cols_from_indeed('Nice')
+b = get_result_content_from_indeed('Nice')
 
-puts a
+puts b
