dfec9b1ce42248e3cb0a7d5fbc4150e097bb4b2e
itokiana <itokiana@gmail.com>
2019-05-28 14:08:57 +0300

    Getted data but not saved to excel

--- scrap_annuairedusport.rb
+++ scrap_annuairedusport.rb
@@ -1,5 +1,6 @@
 require "open-uri"
 require "nokogiri"
+require 'spreadsheet'
 
 def all_links
   doc = Nokogiri::HTML(open("http://www.annuairedusport.fr/resultat.php?numero=13&NOM_COMMUNE=alldep&discipline=allsprt&Handi=0&adapt=0&rch_by_comcom=0&cartoCom&fbclid=IwAR3uW12TFzlo97w-Rjp1EcbNN36pw68ikLIIN5jnth3ZtSP6r0wr9sK3WVk"))
@@ -11,22 +12,49 @@ end
 
 def get_details(link)
   doc = Nokogiri::HTML(open("http://www.annuairedusport.fr/" + link))
-  details = {}
+  details = []
   get_details = doc.css("#nav_asso_detail_body")
-  details = {
-    :federation => get_details.css("center")[1].css("a")[0].content,
-    :headquarter => get_details.css("center")[2].content,
-    :phone => get_details.css("center")[3].css("a")[0] == nil ? get_details.css("center")[3].content : "NON RENSEIGNER",
-    :email => get_details.css("center")[4].css("a")[0] != nil ? get_details.css("center")[4].css("a")[0].content : "NON RENSEIGNER"
-  }
+  federation = get_details.css("center")[1].css("a")[0] != nil ? get_details.css("center")[1].css("a")[0].content : get_details.css("center")[1].content
+  headquarter = get_details.css("center")[2].content
+  phone = get_details.css("center")[3].css("a")[0] == nil ? get_details.css("center")[3].content : "NON RENSEIGNER"
+  email = get_details.css("center")[4] != nil && get_details.css("center")[4].css("a")[0] != nil ? get_details.css("center")[4].css("a")[0].content : "NON RENSEIGNER"
+  more = "http://www.annuairedusport.fr/" + link
+  details.push([
+    federation,
+    headquarter,
+    phone,
+    email,
+    more,
+  ])
+  details
 end
 
 l = all_links()
-# d = []
 
-# l.each do |x|
-#   d.push(get_details(x))
-# end
-d = get_details(l[3])
+d = []
 
-puts d
+l.each do |k|
+  d.push(get_details(k))
+end
+
+
+book = Spreadsheet::Workbook.new
+bold = Spreadsheet::Format.new :weight => :bold
+sheet = book.create_worksheet :name => 'annuaire du sport'
+sheet.row(0).set_format(0, bold)
+sheet.row(0).set_format(1, bold)
+sheet.row(0).set_format(2, bold)
+sheet.row(0).set_format(3, bold)
+sheet.row(0).set_format(4, bold)
+sheet.row(0).push "Fédération","Siège social", "Téléphone", "E-mail", "Détails"
+
+lastRow = sheet.last_row_index + 1
+d.each_with_index do |row, rowNum|
+      row.each_with_index do |cell, cellNum|
+          sheet[ rowNum + lastRow, cellNum ] = cell
+      end
+end
+book.write 'annuairedusport.xls'
+
+# puts d
+# puts get_details(l[3])
--- test.rb
+++ test.rb
@@ -7,33 +7,53 @@ require 'spreadsheet'
 #   puts node.text
 # end
 
-d = [
-  {:name => "Koto", :age => 23},
-  {:name => "Koto", :age => 23},
-  {:name => "Koto", :age => 23}
-]
+# d = [
+#   {:name => "Koto", :age => 23},
+#   {:name => "Koto", :age => 23},
+#   {:name => "Koto", :age => 23}
+# ]
 
-f = 'test.xls'
+# f = 'test.xls'
+
+# book = Spreadsheet::Workbook.new
+# bold = Spreadsheet::Format.new :weight => :bold
+# sheet1 = book.create_worksheet :name => 'test'
+# sheet1.row(0).set_format(0, bold)
+# sheet1.row(0).set_format(1, bold)
+# sheet1.row(0).push "just text","another text", "hehe"
+# book.write 'test.xls'
+
+
+arrays = [
+['Text 1','Text 2','Text 3'],
+['Text 4','Text 5','Text 6'],
+['Text 7','Text 8','Text 9']]
 
 book = Spreadsheet::Workbook.new
 bold = Spreadsheet::Format.new :weight => :bold
-sheet1 = book.create_worksheet :name => 'test'
-sheet1.row(0).set_format(0, bold)
-sheet1.row(0).set_format(1, bold)
-sheet1.row(0).push "just text","another text"
+sheet = book.create_worksheet :name => 'test'
+sheet.row(0).set_format(0, bold)
+sheet.row(0).set_format(1, bold)
+sheet.row(0).set_format(2, bold)
+sheet.row(0).push "just text","another text", "hehe"
+
+lastRow = sheet.last_row_index + 1
+arrays.each_with_index do |row, rowNum|
+      row.each_with_index do |cell, cellNum|
+          sheet[ rowNum + lastRow, cellNum ] = cell
+      end
+end
 book.write 'test.xls'
-
-
-# arrays = [
-# ['Text 1','Text 2','Text 3'],
-# ['Text 4','Text 5','Text 6'],
-# ['Text 7','Text 8','Text 9']]
 
 # f = './test.xls'
 # Spreadsheet.client_encoding = 'UTF-8'
 # if File.exist? f
 #     book = Spreadsheet.open(f)
+#     bold = Spreadsheet::Format.new :weight => :bold
 #     sheet = book.worksheet(0)
+#     sheet.row(0).set_format(0, bold)
+#     sheet.row(0).set_format(1, bold)
+#     sheet.row(0).push "just text","another text", "hehe"
 
 #     lastRow = sheet.last_row_index + 1
 #     arrays.each_with_index do |row, rowNum|
